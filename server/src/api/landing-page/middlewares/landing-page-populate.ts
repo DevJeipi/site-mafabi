94ae75fb8a39bb300518cb19e8d808e0a2bd91ab
DevJeipi <joaopedroghilardi@gmail.com>
2026-04-07 22:30:03 -0300

    Convert server from submodule to regular directory

---
+++ server/src/api/landing-page/middlewares/landing-page-populate.ts
@@ -0,0 +1,33 @@
+/**
+ * `landing-page-populate` middleware
+ */
+
+import type { Core } from "@strapi/strapi";
+
+const populate = {
+  blocks: {
+    on: {
+      "blocks.hero": {
+        populate: {
+          links: true,
+          image: {
+            fields: ["alternativeText", "url"],
+          },
+          imageAbove: {
+            fields: ["alternativeText", "url"],
+          },
+        },
+      },
+    },
+  },
+};
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info("In landing-page-populate middleware.");
+    ctx.query.populate = populate;
+
+    await next();
+  };
+};
